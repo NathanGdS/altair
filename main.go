cd458b762eba077721d43688408378337354dbb2
Nathan G. Santos <nathangar01@gmail.com>
2025-08-05 22:52:59 -0300

    feat: publish http handler

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	Id         string         `json:"id"`
+	Data       map[string]any `json:"data"`
+	ReceivedAt time.Time      `json:"received_at"`
+}
+
+func (m *Message) Instantiate() {
+	m.Id = uuid.New().String()
+	m.ReceivedAt = time.Now().UTC()
+}
+
+func PublishHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{}
+
+	json.NewDecoder(r.Body).Decode(&message)
+
+	message.Instantiate()
+
+	fmt.Println(message)
+
+	jsonBytes, err := json.Marshal(message)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("POST /publish", PublishHandler)
+
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
